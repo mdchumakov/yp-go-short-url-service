86e4a730a8d6268458ac5e3e86dee3cc0967f003
chumakov-md <chumakov-md@zvuk.com>
2025-08-20 03:00:48 +0300

    Refactor SQLite repository initialization and cleanup SQL scripts for consistency

--- migrations/000002_add_user_id_to_urls.down.sql
+++ migrations/000002_add_user_id_to_urls.down.sql
@@ -4,5 +4,5 @@ DROP INDEX IF EXISTS idx_user_urls_url_id;
 DROP INDEX IF EXISTS idx_user_urls_user_id;
 
 -- Удаляем таблицы
-DROP TABLE IF EXISTS user_urls CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXISTS user_urls CASCADE;
+-- DROP TABLE IF EXISTS users CASCADE;
